2969048670a44d5b63cf3b11dad7386b781f808d
KomCat23 <artemkurnasenko2010@gmail.com>
2026-05-24 12:20:41 +0300

    Add files via upload

---
+++ calc.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+	int a;
+	cin >> a;
+	int b;
+	cin >> b;
+	char c[99];
+	cin >> c;
+	if(strcmp(c, "plus") == 0 || (c, "+") == 0){
+		int f = a+b;
+		cout << f << endl;
+	}
+	else if(strcmp(c, "-") == 0 || (c, "minus") == 0){
+		int f = a-b;
+		cout << f << endl;
+	}
+	else if(strcmp(c, "*") == 0 || (c, "multiple") == 0){
+		int f = a*b;
+		cout << f << endl;
+	}
+	else if(strcmp(c, "**") == 0){
+		long f = pow(a,b);
+		cout << f << endl;
+	}
+	return 0;
+}
